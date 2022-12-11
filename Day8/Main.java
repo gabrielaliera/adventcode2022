f63e235e2c1bb319b1db2db0dff1d6bb1963f74a
Gabriela Liera <95945032+gabrielaliera@users.noreply.github.com>
2022-12-11 23:31:54 +0000

    Created recursive isVisible and checkVisibility methods

--- Day8/Main.java
+++ Day8/Main.java
@@ -6,6 +6,7 @@ public class Main{
 
   static ArrayList<String> data = new ArrayList<>();
   static int[][] grid;
+  static int count=0;
   
   public static void readFile(String fileName){
     try{
@@ -54,11 +55,48 @@ public class Main{
     }
   }
 
-   public static void main(String[] args) {        
+  public static void isVisible(){
+     // boolean vis =checkVisiblity(0, 0, grid[0][0]);
+     // if(vis){
+     //     //   count++;
+     //      }
+    for(int i=0;i<grid.length;i++){
+      for(int j=0;j<grid[0].length;j++){
+          boolean vis =checkVisiblity(i, j, grid[i][j]);
+          if(vis){
+            //count++;
+          }
+      }
+    }
+    
+  }
+
+  public static boolean checkVisiblity(int row, int col, int value){
+
+    if(row < 0 || row>=grid.length || col< 0 || col>=grid[0].length || grid[row][col] < value){
+     // count++;
+      return true;
+    }
+    boolean above = checkVisiblity(row-1,col,value); //above
+    boolean below = checkVisiblity(row+1,col,value); //below
+    boolean left = checkVisiblity(row,col-1,value); //left
+    boolean right = checkVisiblity(row,col+1,value); //right
+
+    if(above || below || left || right){
+      count++;
+    }
+    
+    //return above || below || left || right;
+    return false;
+  }
+
+  public static void main(String[] args) {        
 
     readFile("Day8/grid.txt");
     createMatrix();
-    printMatrix();
+    //printMatrix();
+    isVisible();
+    System.out.println(count);
     
     System.out.println("Hello world!");
   }
