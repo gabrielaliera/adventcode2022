110c34ae5f090eb0cdeef05e59d7e07021293618
Gabriela Liera <95945032+gabrielaliera@users.noreply.github.com>
2022-12-12 04:31:23 +0000

    changed recursion to iterative

--- Day8/Main.java
+++ Day8/Main.java
@@ -48,9 +48,9 @@ public class Main{
 
   public static void printMatrix(){
     for(int i=0;i< grid.length;i++){
-      System.out.println(i);
+      System.out.println();
       for(int j=0;j<grid[0].length;j++){
-        System.out.print(grid[i][j]+" ");
+        System.out.print(grid[i][j]);
       }
     }
   }
@@ -62,29 +62,53 @@ public class Main{
      //      }
     for(int i=0;i<grid.length;i++){
       for(int j=0;j<grid[0].length;j++){
+        System.out.println(i+" "+j);
           boolean vis =checkVisiblity(i, j, grid[i][j]);
-          if(vis){
-            //count++;
-          }
+          if(vis){count++;}
+      //  System.out.println(i+" "+j);
+        //System.out.println(vis);  
+       // System.out.println("gaby");
       }
     }
     
   }
 
+  public static void checkAbove(int row,int col, int value){
+    while(row>= 0){
+      if(grid[row][col]>value)
+      row--;
+    }
+  }
+
   public static boolean checkVisiblity(int row, int col, int value){
 
-    if(row < 0 || row>=grid.length || col< 0 || col>=grid[0].length || grid[row][col] < value){
-     // count++;
+    // if(row < 0 || row>=grid.length || col< 0 || col>=grid[0].length || grid[row][col] < value){
+    //  //System.out.println("tryerw");
+    //   return true;
+    // }
+  //  System.out.println("asd");
+    // boolean above = checkVisiblity(row-1,col,value); //above
+    // boolean below = checkVisiblity(row+1,col,value); //below
+    // boolean left = checkVisiblity(row,col-1,value); //left
+    // boolean right = checkVisiblity(row,col+1,value); //right
+
+    if(row-1< 0 || grid[row-1][col]<value){
       return true;
     }
-    boolean above = checkVisiblity(row-1,col,value); //above
-    boolean below = checkVisiblity(row+1,col,value); //below
-    boolean left = checkVisiblity(row,col-1,value); //left
-    boolean right = checkVisiblity(row,col+1,value); //right
-
-    if(above || below || left || right){
-      count++;
+    if(row+1 >= grid.length || grid[row+1][col]<value){
+      return true;
     }
+    if(col-1<  0 || grid[row][col-1]<value){
+      return true;
+    }
+    
+;    if(col+1 >= grid[0].length || grid[row][col+1]<value){
+      return true;
+    }
+
+    // if(above || below || left || right){
+    //   count++;
+    // }
     
     //return above || below || left || right;
     return false;
@@ -95,9 +119,10 @@ public class Main{
     readFile("Day8/grid.txt");
     createMatrix();
     //printMatrix();
+    System.out.println(grid[95][98]);
     isVisible();
     System.out.println(count);
-    
+    System.out.println(grid[0].length);
     System.out.println("Hello world!");
   }
 }
