75b039302127107969c0d9ab810d15f298d640df
Gabriela Liera <95945032+gabrielaliera@users.noreply.github.com>
2022-12-13 23:25:17 +0000

    Created addScoreToQueue method

--- Day10/Main.java
+++ Day10/Main.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 
 /*
 addx V takes two cycles to complete. After two cycles, the X register is increased by the value V. (V can be negative.)
@@ -11,6 +13,8 @@ noop takes one cycle to complete. It has no other effect.
 public class Main{
   
   static ArrayList<String> data = new ArrayList<>();
+  static Queue<Integer>  scores = new LinkedList<>();
+  static Queue<Integer> cycels = new LinkedList<>();
 
   public static void readFile(String fileName){
     try{
@@ -28,16 +32,22 @@ public class Main{
   }
 
   public static void cycles(){
-    
+    int X = 0;
     //for(String line : data){
       for(int i=0; i<5;i++){
         String line = data.get(i);
         if(line.equals("noop")){
           
         }else {
-          
+          addScoreToQueue(line);
         }
     }
+  }
+
+  public static void addScoreToQueue(String line){
+    String[] instructions = line.split(" ");
+    int score = Integer.valueOf(instructions[1]);
+    scores.add(score);
   }
    public static void main(String[] args) {        
 
